958656424bed32b12b4ce6b89e77d0555ab34abc
Vishal Sharbidar Mukunda <vishalsharbidar@gmail.com>
2023-06-19 20:27:47 +0200

    code improvement

--- demo_SphereGlue.py
+++ demo_SphereGlue.py
@@ -15,19 +15,19 @@ torch.manual_seed(1)
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='SuperGlue',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--input', type=str, nargs='+', default='data/', 
+    parser.add_argument('--input', type=str, default='data/', 
         help=' Input path', metavar='')   
-    parser.add_argument('--output', type=str, nargs='+', default='output/', 
-        help=' Output path', metavar='')                                                                            
+    parser.add_argument('--output', type=str, default='output/', 
+        help=' Output path', metavar='') 
+    parser.add_argument('--images', type=str, default='images/', 
+        help=' Image path', metavar='')                                                                           
     parser.add_argument('--match_threshold', type=float, default=0.2,
         help=' Match threshold ', metavar='')
     parser.add_argument('--batch_size', type=int, default=1,
         help=' Batch size of training images', metavar='')
-    parser.add_argument('--knn', type=int, default=20,
+    parser.add_argument('--knn', type=int, default=40,
         help=' K nearest neighbour for creating edges', metavar='')
-    parser.add_argument('--K', type=int, default=2,
-        help=' Chebyshev filter size K', metavar='')
-    parser.add_argument('--sinkhorn_iterations', type=int, default=20,
+    parser.add_argument('--sinkhorn_iterations', type=int, default=40,
         help=' Sinkhorn iterations', metavar='')
     parser.add_argument('--GNN_layers', type=str, nargs='+', default=['cross'],
         help=' GNN layers', metavar='') # ['self', 'cross']
@@ -46,9 +46,9 @@ if __name__ == '__main__':
     print('\nRunning inference on device \"{}\"'.format(device))
 
     # Config file
-    default_config = {'K': args.K, 
+    default_config = {'K': 2, #Chebyshev filter size
+                    'GNN_layers': ['cross'], 
                     'match_threshold': args.match_threshold,
-                    'GNN_layers':args.GNN_layers, 
                     'sinkhorn_iterations':args.sinkhorn_iterations,
                     'aggr': args.aggregation,
                     'knn': args.knn,
@@ -79,8 +79,6 @@ if __name__ == '__main__':
     ckpt_data = torch.load(model_path)
     matching_test.load_state_dict(ckpt_data["MODEL_STATE_DICT"])
     matching_test.eval()
-    
-
     print('Loading the model weights for detector ' + str(args.detector ) +  ' from->', model_path)
     
 
@@ -88,11 +86,16 @@ if __name__ == '__main__':
         for data, y_true in data_loader:
             # run the model on the given dataset
             y_pred = matching_test(data)
-            output = args.output  + str(data['name'][0]) 
-            out = {'correspondences': y_pred['matches0'][0].to(torch.float32).detach().cpu().numpy(),
+            
+            # Output path
+            output_path = os.path.join(args.output, args.detector)
+            if not os.path.exists(output_path):
+                os.mkdir(output_path)
+
+            output_data = {'correspondences': y_pred['matches0'][0].to(torch.float32).detach().cpu().numpy(),
                    'scores': y_pred['matching_scores0'][0].to(torch.float32).detach().cpu().numpy()}
 
-            numpy.savez(output, **out)
+            numpy.savez(os.path.join(output_path,str(data['name'][0])), **output_data)
 
     print('Predictions are saved in -> ', args.output)
 
